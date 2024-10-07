ab907019d436be043ae242b1bce5f18572f4ea6c
Transportistas <jmonterom20@educarex.es>
2024-10-07 21:37:40 +0200

    Added some things.

--- app/src/main/java/com/example/domoticon/MainActivity.java
+++ app/src/main/java/com/example/domoticon/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.domoticon;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -8,6 +12,7 @@ import com.example.domoticon.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
     ActivityMainBinding binding;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +22,25 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
+        String[] valores = {"2", "3"};
+        ArrayAdapter<String> adaptador = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, valores);
 
+        binding.spinnerSelector.setAdapter(adaptador);
+        binding.spinnerSelector.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                mensaje("Seleccionado " + adapterView.getAdapter().getItem(i));
+                adapterView.getAdapter().getItem(i);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+                mensaje("Sin selección");
+            }
+        });
+    }
+
+    private void mensaje(String texto) {
+        Toast.makeText(this, texto, Toast.LENGTH_SHORT).show();
     }
 }
